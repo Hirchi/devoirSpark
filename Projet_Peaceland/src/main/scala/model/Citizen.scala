e6d61a4e3bc9679e584f9eacbbaf29d4451e4cb9
Hirchi <48589267+Hirchi@users.noreply.github.com>
2022-03-18 14:05:14 +0100

    Add files via upload

---
+++ Projet_Peaceland/src/main/scala/model/Citizen.scala
@@ -0,0 +1,25 @@
+import scala.util.Random
+import faker._
+Case class Citizen(citizenName : String, peaceScore: Int){
+    val name = citizenName
+    val score = peaceScore
+    def toString(): String = {
+        val str_to_return = "" + name + ":" + peacescore
+        str_to_return
+    }
+}
+object Citizen{
+    def createRandomCitizen() = {
+
+      val name = Name.name
+      
+      val start = 0
+      val end = 100
+      val numberRandom = new scala.util.Random
+      val peacescore = start + numberRandom.nextInt(( end - start ) + 1 )
+
+      Citizen(name, peacescore)
+
+    }
+  
+}
